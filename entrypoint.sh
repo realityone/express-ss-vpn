1377764542679efc37272aac48e8dba0eaa83e31
realityone <realityone@me.com>
2019-11-20 21:18:45 +0800

    add ingress ip route

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,10 @@ echo "nameserver 8.8.8.8\nnameserver 8.8.4.4\n" > /etc/resolv.conf
 gateway=`ip route get 8.8.8.8 | grep src | awk '{print $3}'`
 
 iptables -A OUTPUT -t mangle -p tcp --sport 8388 -j MARK --set-mark 0x1
-iptables -A OUTPUT -t mangle -p udp --sport 8388 -j MARK --set-mark 0x1
+# iptables -A OUTPUT -t mangle -p udp --sport 8388 -j MARK --set-mark 0x1
+if [[ ! -z $INGRESS_IP ]]; then
+    ip route add $INGRESS_IP via $gateway dev eth0
+fi
 
 ip route add table 100 default via $gateway dev eth0
 ip rule add fwmark 0x1 table 100
